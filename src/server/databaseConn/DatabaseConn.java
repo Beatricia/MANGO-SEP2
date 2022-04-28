b188da0b0119eaad2a6075d99c587fe1325584f9
Enbi81 <wotng2002@gmail.com>
2022-04-28 12:45:49 +0200

    Added Javadocs to the Database Connection class and interface

--- src/server/databaseConn/DatabaseConn.java
+++ src/server/databaseConn/DatabaseConn.java
@@ -6,10 +6,32 @@ import util.LogInException;
 
 import java.sql.SQLException;
 
+/**
+ * Abstract Model for accessing the Database.
+ */
 public interface DatabaseConn
 {
+  /**
+   * Log in with the specified username and the password.
+   * @param username username
+   * @param password password
+   * @return A user object representing the logged-in user if the log-in was successful
+   * @throws SQLException When an unexpected sql exception happens
+   * @throws LogInException When the user has not provided the correct data
+   */
   User login(String username, String password) throws SQLException,
       LogInException;
+
+  /**
+   * Register a user with the specified username, password, first name and last name.
+   * @param firstName first name
+   * @param lastName last name
+   * @param username username
+   * @param password password
+   * @param userType type of the user
+   * @return A user object representing the registered user if the register was successful
+   * @throws SQLException When the user has not provided the correct data
+   */
   User register(String firstName, String lastName, String username, String password, UserType userType) throws
       SQLException;
 }
