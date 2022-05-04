aa4710b71c6c55c879a24ef297b3a45d67ae50b8
Enbi81 <wotng2002@gmail.com>
2022-05-04 14:40:56 +0200

    Solid ViewHandler

--- src/client/core/ViewHandler.java
+++ src/client/core/ViewHandler.java
@@ -2,15 +2,18 @@ package client.core;
 
 import client.view.Login.LoginViewController;
 import client.view.Register.RegisterViewController;
+import client.view.ViewController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 /**
  * A class that handles the opening of the fxml view files
+ *
  * @author Uafa
  */
 public class ViewHandler
@@ -18,13 +21,17 @@ public class ViewHandler
   private ViewModelFactory viewModelFactory;
   private Stage stage;
 
+  public Stage getStage() {
+    return stage;
+  }
+
   /**
    * Constructor for the class
-   * @param viewModelFactory  a viewModelFactory object
-   * @param stage  a Stage object
+   *
+   * @param viewModelFactory a viewModelFactory object
+   * @param stage            a Stage object
    */
-  public ViewHandler(ViewModelFactory viewModelFactory, Stage stage)
-  {
+  public ViewHandler(ViewModelFactory viewModelFactory, Stage stage) {
     this.viewModelFactory = viewModelFactory;
     this.stage = stage;
   }
@@ -33,9 +40,10 @@ public class ViewHandler
    * A method that is responsible for opening the login view, which is the first
    * view shown when the program start
    */
-  public void start()
-  {
-    openLoginView();
+  public void start() {
+    //openLoginView();
+
+    openAddDishesView();
   }
 
   /**
@@ -43,28 +51,13 @@ public class ViewHandler
    * calls the init() method in the controller, sets a title for the stage and
    * displays the view.
    */
+  public void openLoginView() {
 
-  public void openLoginView()
-  {
-    FXMLLoader loader = new FXMLLoader();
-    loader.setLocation(getClass().getResource("../view/Login/LoginView.fxml"));
+    String path = "../view/Login/LoginView.fxml";
+    Pane p = openView(path);
 
-    Parent root = null;
-
-    try
-    {
-      root = loader.load();
-    }
-    catch (IOException e)
-    {
-      e.printStackTrace();
-    }
-
-    LoginViewController controller = loader.getController();
-    controller.init(this, viewModelFactory);
     stage.setTitle("Log in");
-
-    Scene scene =new Scene(root);
+    Scene scene = new Scene(p);
     stage.setScene(scene);
     stage.show();
   }
@@ -75,56 +68,43 @@ public class ViewHandler
    * title for the stage and displays the view.
    */
 
-  public void openRegisterView()
-  {
-    FXMLLoader loader = new FXMLLoader();
+  public void openRegisterView() {
+    String path = "../view/Register/RegisterViewNew.fxml";
+    Pane p = openView(path);
 
-    loader.setLocation(getClass().getResource("../view/Register/RegisterViewNew.fxml"));
-
-    Parent root = null;
-
-    try
-    {
-      root = loader.load();
-    }
-    catch (IOException e)
-    {
-      e.printStackTrace();
-    }
-
-    RegisterViewController controller = loader.getController();
-    controller.init(this, viewModelFactory);
     stage.setTitle("Register");
-
-
-    Scene scene =new Scene(root);
+    Scene scene = new Scene(p);
     stage.setScene(scene);
     stage.show();
   }
 
-    public void openAddDishesView() {
-      FXMLLoader loader = new FXMLLoader();
+  public void openAddDishesView() {
+    String path = "../view/MenuEmpl/MenuEmpl.fxml";
+    Pane p = openView(path);
 
-      loader.setLocation(getClass().getResource("../view/MenuEmpl/MenuEmpl.fxml"));
+    stage.setTitle("Add items to the menu");
+    Scene scene = new Scene(p);
+    stage.setScene(scene);
+    stage.show();
+  }
 
-      Parent root = null;
+  private Pane openView(String path) {
+    FXMLLoader loader = new FXMLLoader();
 
-      try
-      {
-        root = loader.load();
-      }
-      catch (IOException e)
-      {
-        e.printStackTrace();
-      }
+    loader.setLocation(getClass().getResource(path));
 
-      RegisterViewController controller = loader.getController();
-      controller.init(this, viewModelFactory);
-      stage.setTitle("Add items to the menu");
+    Pane root = null;
 
-
-      Scene scene =new Scene(root);
-      stage.setScene(scene);
-      stage.show();
+    try {
+      root = loader.load();
     }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    ViewController controller = loader.getController();
+    controller.init(this, viewModelFactory);
+
+    return root;
+  }
 }
