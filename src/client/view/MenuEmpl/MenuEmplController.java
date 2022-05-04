a4206b7bfd2f1b37fc055e4670530a9a6b6f504a
Beatricia <315186@via.dk>
2022-05-04 09:36:15 +0200

    Added javadocs for MenuEmplController

--- src/client/view/MenuEmpl/MenuEmplController.java
+++ src/client/view/MenuEmpl/MenuEmplController.java
@@ -5,26 +5,38 @@ import client.core.ViewModelFactory;
 import client.view.ViewController;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.util.List;
+
+/**
+ * Class responsible for connecting MenuEmpl.fxml with the MenuEmplViewModel and therefore providing functionality to the GUI.
+ * A controller for the employer's menu where he can add new items.
+ * @author beatricia
+ * @version 1
+ */
 
 public class MenuEmplController implements ViewController {
     public TextField nameTextField;
-    public TextField ingredientsTextField;
     public TextField priceTextField;
     public ImageView imageView;
     public Label errorLabel;
+    public TextArea ingredientsAreaField;
 
     public ViewHandler viewHandler;
     public MenuEmplViewModel viewModel;
     private File lastSelected;
     private String pictureFile;
 
+    /**
+     * Binding all necessary fields with the MenuEmplViewModel and initializing the view Handler
+     * @param viewHandler instance of ViewHandler class, which is responsible for managing the GUI views
+     * @param viewModelFactory instance of ViewModelFactory class, where ViewModels are created
+     */
     @Override
     public void init(ViewHandler viewHandler, ViewModelFactory viewModelFactory) {
         this.viewHandler = viewHandler;
@@ -33,15 +45,23 @@ public class MenuEmplController implements ViewController {
         errorLabel.textProperty().bindBidirectional(viewModel.getErrorMessage());
     }
 
+    /**
+     * Adding a new item to the database with it's name, ingredients and price
+     * @param actionEvent from GUI(Button click)
+     */
     public void addButton(ActionEvent actionEvent) {
-        viewModel.addItem(nameTextField.getText(), ingredientsTextField.getText(),priceTextField.getText());
+        viewModel.addItem(nameTextField.getText(), ingredientsAreaField.getText(),priceTextField.getText());
     }
 
+    /**
+     * Importing a picture from the PC
+     * @param actionEvent from GUI(Button click)
+     */
     public void selectButton(ActionEvent actionEvent) {
         //create new file chooser
         FileChooser fileChooser = new FileChooser();
         //add extension filter, the user can only select png, jpeg
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(".PFG",".JPEG"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(".PNG",".JPEG"));
         fileChooser.setTitle("Selecting a picture");
 
         if(lastSelected!=null){
