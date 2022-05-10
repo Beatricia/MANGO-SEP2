43088f0caf38e102ff12c018a4a21b79096a171a
Enbi81 <wotng2002@gmail.com>
2022-05-10 03:41:43 +0200

    Created View for Accept Employee

---
+++ src/client/view/Admin/acceptEmployee/AcceptEmployeeViewModel.java
@@ -0,0 +1,19 @@
+package client.view.Admin.acceptEmployee;
+
+import javafx.collections.ObservableList;
+import transferobjects.User;
+
+public class AcceptEmployeeViewModel
+{
+  public void handleUser(User user, boolean accept){
+
+  }
+
+  public void refresh(){
+
+  }
+
+  public ObservableList<User> getEmployeeList(){
+    return null;
+  }
+}
