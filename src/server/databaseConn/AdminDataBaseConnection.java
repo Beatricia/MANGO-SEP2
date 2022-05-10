9cf90747a4c1412874dd9a845526cad30e5bacbd
Beatricia <315186@via.dk>
2022-05-10 11:36:32 +0200

    Updated the AdminDataBaseConnection.java the handlePendingEmployee method

--- src/server/databaseConn/AdminDataBaseConnection.java
+++ src/server/databaseConn/AdminDataBaseConnection.java
@@ -10,15 +10,51 @@ import java.util.ArrayList;
 public class AdminDataBaseConnection {
     public void handlePendingEmployee(String username, boolean accept) throws SQLException {
         try(Connection connection = DatabaseConnImp.getConnection()) {
-            String str = "INSERT INTO EMPLOYEE_PENDING (username, accept) VALUES (?, ?);";// DO WE INSERT IN THE EXISTING TABLE ?
-            PreparedStatement statement = connection.prepareStatement(str);
-
-            statement.setString(1,username);
-            statement.setBoolean(2, accept);
+            PreparedStatement statement;
+            if(accept) {
+                String str = "UPDATE employee SET accepted = true\n" +
+                        "WHERE username = " + username;
+                statement = connection.prepareStatement(str);
+            } else {
+                String str = "DELETE from employee\n" +
+                        "where username = " + username;
+                statement = connection.prepareStatement(str);
+            }
+            statement.execute();
         }
     }
 
     public ArrayList<User> getAllPendingEmployees() throws SQLException {
+        ArrayList<User> pendingEmployees;
+        try(Connection connection = DatabaseConnImp.getConnection()) {
+            String str = "SELECT username, employee.accepted\n" +
+                    "FROM employee\n" +
+                    "WHERE employee.accepted=false;";
+            PreparedStatement statement = connection.prepareStatement(str);
+            statement.execute();
+        }
         return null;
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws SQLException {
+        boolean accept = true;
+        String username = "Greg";
+
+        try(Connection connection = DatabaseConnImp.getConnection()) {
+            PreparedStatement statement;
+            if(accept) {
+                String str = "UPDATE employee SET accepted = true\n" +
+                        "WHERE username = '" + username + "'";
+                statement = connection.prepareStatement(str);
+            } else {
+                String str = "DELETE from employee\n" +
+                        "where username = '" + username +"'";
+                statement = connection.prepareStatement(str);
+            }
+            statement.execute();
+        }
+
     }
 }
