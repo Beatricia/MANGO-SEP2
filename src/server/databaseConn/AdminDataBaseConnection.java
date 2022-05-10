a81567f6b3fe98bb6d35918349dc381cf5496130
Beatricia <315186@via.dk>
2022-05-10 13:21:14 +0200

    Updated the AdminDataBaseConnection.java the getAllPendingEmployees

--- src/server/databaseConn/AdminDataBaseConnection.java
+++ src/server/databaseConn/AdminDataBaseConnection.java
@@ -1,9 +1,11 @@
 package server.databaseConn;
 
+import shared.UserType;
 import transferobjects.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -25,35 +27,50 @@ public class AdminDataBaseConnection {
     }
 
     public ArrayList<User> getAllPendingEmployees() throws SQLException {
-        ArrayList<User> pendingEmployees;
+        ArrayList<User> pendingEmployees = new ArrayList<>();
         try(Connection connection = DatabaseConnImp.getConnection()) {
-            String str = "SELECT username, employee.accepted\n" +
+            String str = "SELECT username, firstName, lastName, employee.accepted\n" +
                     "FROM employee\n" +
                     "WHERE employee.accepted=false;";
             PreparedStatement statement = connection.prepareStatement(str);
-            statement.execute();
+            ResultSet set = statement.executeQuery();
+            if(set.next())
+            {
+                String userName = set.getString("username");
+                String firstName = set.getString("firstname");
+                String lastName = set.getString("lastname");
+                boolean accepted = set.getBoolean("accepted");
+                if(accepted == false){
+                    User user = new User(userName, UserType.EMPLOYEE, firstName, lastName);
+                    pendingEmployees.add(user);
+                }
+            }
+            return pendingEmployees;
         }
-        return null;
     }
 }
 
 class Main {
     public static void main(String[] args) throws SQLException {
-        boolean accept = true;
-        String username = "Greg";
-
+        ArrayList<User> pendingEmployees = new ArrayList<>();
         try(Connection connection = DatabaseConnImp.getConnection()) {
-            PreparedStatement statement;
-            if(accept) {
-                String str = "UPDATE employee SET accepted = true\n" +
-                        "WHERE username = '" + username + "'";
-                statement = connection.prepareStatement(str);
-            } else {
-                String str = "DELETE from employee\n" +
-                        "where username = '" + username +"'";
-                statement = connection.prepareStatement(str);
+            String str = "SELECT username, firstName, lastName\n" +
+                    "FROM employee\n" +
+                    "WHERE employee.accepted=false;";
+            PreparedStatement statement = connection.prepareStatement(str);
+            ResultSet set = statement.executeQuery();
+            while(set.next())
+            {
+                String userName = set.getString("username");
+                String firstName = set.getString("firstname");
+                String lastName = set.getString("lastname");
+                    User user = new User(userName, UserType.EMPLOYEE, firstName, lastName);
+                    pendingEmployees.add(user);
+
             }
-            statement.execute();
+            for (int i = 0; i < pendingEmployees.size(); i++) {
+                System.out.println(i+" " + pendingEmployees.get(i)+"\n");
+            }
         }
 
     }
