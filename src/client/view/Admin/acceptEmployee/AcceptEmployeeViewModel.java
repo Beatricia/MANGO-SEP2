07b7d6da1416ddf1f74edc1df3abcf7504a05717
simonfrayer <89986425+simonfrayer@users.noreply.github.com>
2022-05-10 11:34:03 +0200

    AcceptEmployeeViewModel implemented

--- src/client/view/Admin/acceptEmployee/AcceptEmployeeViewModel.java
+++ src/client/view/Admin/acceptEmployee/AcceptEmployeeViewModel.java
@@ -1,19 +1,59 @@
 package client.view.Admin.acceptEmployee;
 
+import client.model.AdminModel;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import shared.UserType;
 import transferobjects.User;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
 public class AcceptEmployeeViewModel
 {
-  public void handleUser(User user, boolean accept){
+  private AdminModel adminModel;
+  private ObservableList<User> users;
+  private static String PENDING_USERS_RECEIVED = "PendingUsersReceived";
+
+  public AcceptEmployeeViewModel(AdminModel adminModel)
+  {
+    this.adminModel = adminModel;
+    users = FXCollections.observableList(new ArrayList<>());
+  }
+
+  public void handleUser(User user, boolean accept)
+  {
+    if (accept)     // accepts employee
+    {
+      adminModel.acceptEmployee(user);
+    }
+    else            //declines employee
+    {
+      adminModel.declineEmployee(user);
+    }
+    users.remove(user);
+  }
+
+  public void refresh()
+  {
+    users.clear();
+
+      ArrayList<User> pendingUsers = adminModel.requestPendingEmployees();
+
+      for (User pendingUser: pendingUsers
+           )
+      {
+        User user = new User(pendingUser.getUsername(), UserType.EMPLOYEE,
+            pendingUser.getFirstName(), pendingUser.getLastName());
+
+        users.add(user);
+      }
 
   }
 
-  public void refresh(){
-
-  }
-
-  public ObservableList<User> getEmployeeList(){
-    return null;
+  public ObservableList<User> getEmployeeList()
+  {
+    return users;
   }
 }
