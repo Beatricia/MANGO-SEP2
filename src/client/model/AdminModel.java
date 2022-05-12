02bb30948e233fa375d888f7442e5e4fa98330e9
simonfrayer <89986425+simonfrayer@users.noreply.github.com>
2022-05-12 08:15:33 +0200

    Javadocs

--- src/client/model/AdminModel.java
+++ src/client/model/AdminModel.java
@@ -3,12 +3,40 @@ package client.model;
 import transferobjects.User;
 import util.PropertyChangeSubject;
 
+import java.beans.PropertyChangeEvent;
 import java.util.ArrayList;
 
+/**
+ * An interface which is responsible for handling the connection between the
+ * ViewModels and the Networking
+ * @author Simon
+ */
 public interface AdminModel extends PropertyChangeSubject
 {
 
+  /**
+   * Method used for creating and sending a Request object to the database (to receive list of pending employees)
+   */
   void requestPendingEmployees();
+
+  /**
+   * This method is responsible for creating and sending a new Request object to the database (to accept an employee)
+   *
+   * @param user one particular User object to be accepted
+   */
   void acceptEmployee(User user);
+
+  /**
+   * This method is responsible for creating and sending a new Request object to the database (to decline an employee)
+   *
+   * @param user one particular User object to be declined
+   */
   void declineEmployee(User user);
+
+  /**
+   * This method is responsible for sending a PropertyChange event to the ViewModel
+   *
+   * @param e event caught from the client
+   */
+  void updatePendingEmployees(PropertyChangeEvent e);
 }
