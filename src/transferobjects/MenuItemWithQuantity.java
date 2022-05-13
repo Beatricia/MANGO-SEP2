d696a1066b92fec7a98e6fde14f7e80f4b146c0b
Beatricia <315186@via.dk>
2022-05-13 12:00:13 +0200

    Implemented MenuItemWithQuantity

---
+++ src/transferobjects/MenuItemWithQuantity.java
@@ -0,0 +1,26 @@
+package transferobjects;
+
+import java.time.LocalDate;
+
+public class MenuItemWithQuantity extends MenuItem {
+    private LocalDate date;
+    private int quantity;
+
+    public MenuItemWithQuantity(MenuItem item, LocalDate date, int quantity ) {
+        super(item.getName(), item.getIngredients(), item.getPrice());
+        this.date=date;
+        this.quantity=quantity;
+    }
+
+    public LocalDate getDate(){
+        return  date;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+}
