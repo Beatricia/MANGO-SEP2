8e12efa98f5f9d3b8daa10438a575eb8adfe5ef8
Enbi81 <wotng2002@gmail.com>
2022-05-18 21:22:49 +0200

    Finishing CartModel interface

--- src/client/model/CartModel.java
+++ src/client/model/CartModel.java
@@ -1,4 +1,15 @@
 package client.model;
 
-public interface CartModel {
+import transferobjects.CartItem;
+import util.PropertyChangeSubject;
+
+public interface CartModel extends PropertyChangeSubject
+{
+  String CART_LIST_RECEIVED = "CartListReceived";
+
+  void addToCart(CartItem cartItem);
+  void editCartItem(CartItem cartItem);
+  void deleteCartItem(CartItem cartItem);
+
+  void requestCartList();
 }
