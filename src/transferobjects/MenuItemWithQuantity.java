fb4d8f28fed68cf3d834019336120b98abc06d4f
Beatricia <315186@via.dk>
2022-05-19 13:19:57 +0200

    Added javadocs for MenuItemWithQuantity from transferObjects

--- src/transferobjects/MenuItemWithQuantity.java
+++ src/transferobjects/MenuItemWithQuantity.java
@@ -2,28 +2,55 @@ package transferobjects;
 
 import java.time.LocalDate;
 
+/**
+ * The class a MenuItemWithQuantity that inherits from MenuItem.
+ * It contains a date and a quantity.
+ */
+
 public class MenuItemWithQuantity extends MenuItem {
   private LocalDate date;
   private int quantity;
 
+  /**
+   * The constructor that initializes the super's fields and the date and quantity
+   * @param item the item from the MenuItem
+   * @param date the item's date
+   * @param quantity the item's quantity
+   */
   public MenuItemWithQuantity(MenuItem item, LocalDate date, int quantity ) {
     super(item.getName(), item.getIngredients(), item.getPrice(), item.getImgPath());
     this.date=date;
     this.quantity=quantity;
   }
 
+  /**
+   * Getting the date
+   * @return the date
+   */
   public LocalDate getDate(){
     return  date;
   }
 
+  /**
+   * Setting the quantity
+   * @param quantity the quantity to be set
+   */
   public void setQuantity(int quantity){
     this.quantity=quantity;
   }
 
+  /**
+   * Getting the quantity
+   * @return the quantity
+   */
   public int getQuantity(){
     return quantity;
   }
 
+  /**
+   * A string that contains the name of the MenuItemWithQuantity
+   * @return the name of the MenuItemWithQuantity
+   */
   @Override public String toString()
   {
     return getName();
