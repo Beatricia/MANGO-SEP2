1f25f2c85345651ed5446c659ac774b73281ce9b
Beatricia <315186@via.dk>
2022-05-19 13:05:08 +0200

    Added javadocs for ShoppingCart controller and viewModel

--- src/client/view/customer/shoppingCart/ShoppingCartController.java
+++ src/client/view/customer/shoppingCart/ShoppingCartController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -29,6 +30,10 @@ import java.sql.Struct;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller for ShoppingCartView.fxml
+ * @author Beatricia
+ */
 public class ShoppingCartController implements ViewController {
     @FXML public TableColumn<CartItem, String> nameColumn;
     @FXML public TableColumn<CartItem, Integer> quantityColumn;
@@ -43,6 +48,11 @@ public class ShoppingCartController implements ViewController {
 
     private ShoppingCartViewModel viewModel;
 
+    /**
+     * Initializes the controller
+     * @param viewHandler instance of ViewHandler class, which is responsible for managing the GUI views
+     * @param viewModelFactory instance of ViewModelFactory class, where ViewModels are created
+     */
     @Override
     public void init(ViewHandler viewHandler, ViewModelFactory viewModelFactory) {
         viewModel = viewModelFactory.getCustomerShoppingCartViewModel();
@@ -61,6 +71,9 @@ public class ShoppingCartController implements ViewController {
         });
     }
 
+    /**
+     * Loading all cart items to the table, they are taken from the view model
+     */
     private void loadCartItems(){
         ObservableList<CartItem> cartItems = FXCollections.observableArrayList();
         cartItems.addAll(viewModel.getAllCartItems());
@@ -71,11 +84,18 @@ public class ShoppingCartController implements ViewController {
         }
     }
 
+    /**
+     * If the cartItem is selected in the table it will appear on the right side
+     * with name, price, image and all the specified ingredient
+     * @param cartItem the cart item to be displayed
+     */
     private void selectedItemChange(CartItem cartItem) {
 
         ArrayList<String> ingredients = cartItem.getIngredients();
         nameLabel.setText(cartItem.getName());
         priceLabel.setText(cartItem.getPrice()+" dkk");
+        Image img = new Image(cartItem.getImgPath());
+        imageView.setImage(img);
 
         for (int i = 0; i < ingredients.size(); i++) {
             String ingredient = ingredients.get(i);
@@ -96,15 +116,22 @@ public class ShoppingCartController implements ViewController {
         viewModel.refresh();
     }
 
-    public void onOrderButton(ActionEvent actionEvent) {
+    /**
+     * If the order button is clicked, the onSaveButton method is called in case there are any changes and the placeOrder
+     * method is called on the viewModel
+     */
+    public void onOrderButton() {
         Log.log("Order button has been clicked");
-        CartItem cartItem = cartTable.getSelectionModel().getSelectedItem();
         onSaveButton();
-        ObservableList<CartItem> cartItems = cartTable.getItems();
-        ArrayList<CartItem> cartItems1 = new ArrayList<>(cartItems);
         viewModel.placeOrder();
     }
 
+    /**
+     * If the save button is clicked, the current item where the changes were made is saved in a cartItem,
+     * then the quantity for the cartItem is set through the spinners value.
+     * The list of all unselectedIngredients is cleared for the cartItem and by running through every ingredient we are
+     * checking if it is not ticket, if not, it is added to the unselectedIngredients with their name
+     */
     public void onSaveButton() {
         Log.log("Save button has been clicked in the shopping cart");
         CartItem cartItem = cartTable.getSelectionModel().getSelectedItem();
@@ -123,7 +150,11 @@ public class ShoppingCartController implements ViewController {
         viewModel.editCartItem(cartItem);
     }
 
-    public void onRemoveButton(ActionEvent actionEvent) {
+    /**
+     * If the remove button is clicked, the current item saved in a cartItemToBeDeleted of type CartItem and sent to the
+     * viewModel through the deleteCartItem method
+     */
+    public void onRemoveButton() {
         Log.log("Delete button has been clicked in the shopping cart");
         CartItem cartItemToBeDeleted = cartTable.getSelectionModel().getSelectedItem();
         viewModel.deleteCartItem(cartItemToBeDeleted);
