8479a32f7cdc4ab0d4129b956b9965830499888b
Uafa <315211@via.dk>
2022-05-24 08:28:41 +0200

    CollectOrder view ready

---
+++ src/client/view/employee/CollectOrder/CollectOrderViewModel.java
@@ -0,0 +1,80 @@
+package client.view.employee.CollectOrder;
+
+import client.model.OrderModelCustomer;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import transferobjects.OrderItem;
+
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+
+/**
+ * A view model class responsible for connecting the CollectOrderViewModel and
+ * the OrderModel
+ */
+public class CollectOrderViewModel
+{
+  private OrderModelCustomer orderModel;
+  private ObservableList<ArrayList<OrderItem>> uncollectedOrders;
+
+  /**
+   * A constructor for the class
+   * @param orderModelCustomer the model that the view should be connected to
+   */
+  public CollectOrderViewModel(OrderModelCustomer orderModelCustomer)
+  {
+    this.orderModel = orderModelCustomer;
+    uncollectedOrders = FXCollections.observableArrayList();
+
+    orderModel.addListener(OrderModelCustomer.ALL_UNCOLLECTED_ORDERS_RECEIVED, this:: ordersReceived);
+
+  }
+
+  /**
+   * The method listens to the OrderModel and whenever it fires a
+   * ALL_UNCOLLECTED_ORDERS_RECEIVED property, this method fills in the private
+   * ObservableList<ArrayList<OrderItem>> attribute with the received data.
+   * @param event the event that has been sent by the OrderModel
+   */
+  private void ordersReceived(PropertyChangeEvent event)
+  {
+
+    ArrayList<ArrayList<OrderItem>> orders = (ArrayList<ArrayList<OrderItem>>) event.getNewValue();
+
+    Platform.runLater( () -> {
+
+      uncollectedOrders.clear();
+      uncollectedOrders.addAll(orders);
+    });
+  }
+
+  /**
+   * The method returns all uncollected orders
+   * @return uncollected orders
+   */
+  public ObservableList<ArrayList<OrderItem>> getAllOrders()
+  {
+   return uncollectedOrders;
+  }
+
+  /**
+   * The method request all uncollected orders from the OrderModel
+   */
+  public void requestUncollectedOrders()
+  {
+    orderModel.requestUncollectedOrder();
+  }
+
+  /**
+   * The method takes the code of an order and send it to the OrderModel so the
+   * order can be canceled.
+   * @param orderCode
+   */
+  public void cancelOrder(int orderCode)
+  {
+    orderModel.collectOrder(orderCode);
+  }
+}
