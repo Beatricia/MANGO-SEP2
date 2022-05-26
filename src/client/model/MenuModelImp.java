ebad181267b48e2284ee2ef9e4c0e372a329c5fc
Uafa <315211@via.dk>
2022-05-26 22:52:34 +0200

    Models JavaDocs ready

--- src/client/model/MenuModelImp.java
+++ src/client/model/MenuModelImp.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-//TODO javadocs
+
 
 /**
  * A class that connects the view model and the Client side of networking.
